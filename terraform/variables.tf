37b060eee9cda29a9730334c6068509f8a1fdc9f
Suryansh Srivastava <ss.srivastava.99@gmail.com>
2024-10-20 18:43:43 +0530

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,8 @@ variable "role" {
     sensitive = true
 }
 variable "subnet_id" {
-    default = "subnet-02061f395d0505b4f,subnet-09e00b2fbe4cf8db0"
+    type = list
+    default = ["subnet-02061f395d0505b4f","subnet-09e00b2fbe4cf8db0"]
 }
 variable "instance_types"{
     default = "t2.micro"
