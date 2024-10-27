5bf984e640046b9b34cc7d84914c8f36bdd83a3d
Suryansh Srivastava <ss.srivastava.99@gmail.com>
2024-10-28 00:02:15 +0530

    fix

--- terraform/alb/alb.tf
+++ terraform/alb/alb.tf
@@ -46,29 +46,3 @@ resource "aws_iam_role_policy_attachment" "alb_ingress_policy_attachment" {
   role       = aws_iam_role.alb_ingress_role.name
   policy_arn = aws_iam_policy.alb_ingress_policy.arn
 }
-resource "aws_security_group" "alb_ingress_sg" {
-  name        = "alb-ingress-sg"
-  description = "Security group for ALB ingress controller"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
