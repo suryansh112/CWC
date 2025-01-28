f2b7208ea0d6ce799e3b486d46ee35ed826e706f
Suryansh Srivastava <ss.srivastava.99@gmail.com>
2025-01-28 09:16:20 +0530

    fix

--- terraform/eks/eks.tf
+++ terraform/eks/eks.tf
@@ -23,8 +23,8 @@ resource "aws_eks_node_group" "mynodegroup" {
 
   instance_types = [var.instance_types]
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 2
     min_size     = 1
 
   }
